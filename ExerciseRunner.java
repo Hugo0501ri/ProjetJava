77e7956c0c3d7f47fadbdb2fe89ceac9d2032222
Hugo <hugorivierre@yahoo.fr>
2024-05-22 16:42:36 +0200

    merge

--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -40,5 +40,35 @@ public class ExerciseRunner {
             default:
                 System.out.println("Langage non pris en charge.");
         }
+        private static void runExercise2(int languageChoice, String code) {
+        String fileName;
+        String[] fileNames;
+        switch (languageChoice) {
+            case 1:
+                // Python
+                fileName = "exo2_utilisateur.py";
+                FileModifier.writeToFile(fileName, code);
+                fileNames = new String[]{"Exercices/Exo2/exo2_utilisateur.py"};
+                CodeRunner.runCodeFromFiles(fileNames);
+                break;
+            case 2:
+                // Java
+                // Ajoutez le code pour exécuter l'exercice 1 en Java
+                break;
+            case 3:
+                // JavaScript
+                // Ajoutez le code pour exécuter l'exercice 1 en JavaScript
+                break;
+            case 4:
+                // PHP
+                // Ajoutez le code pour exécuter l'exercice 1 en PHP
+                break;
+            case 5:
+                // C
+                // Ajoutez le code pour exécuter l'exercice 1 en C
+                break;
+            default:
+                System.out.println("Langage non pris en charge.");
+        }
     }
 }
