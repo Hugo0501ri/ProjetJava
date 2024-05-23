a8809f326b060ff44af3b3e39c3054e661e4a3ef
LoloLeRigaulO <loickenmogne16@gmail.com>
2024-05-23 18:45:05 +0200

    tests de programmes

---
+++ Exercices/ExoC/Exo1C/exo1_utilisateur.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+int somme(int x, int y){
+	return x+y;
+}
+void main(){
+	printf("%i", somme(2,2));
+}
---
+++ Exercices/ExoJava/Exo1Java/exo1_utilisateur.java
@@ -0,0 +1,6 @@
+public class Main {
+	public void main(){
+		System.out.println("caca");
+	}
+}
+
