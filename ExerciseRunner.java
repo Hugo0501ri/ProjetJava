6e9ef1492799278a1e8e87ccfd136fc13c4be7ba
Hugo <hugorivierre@yahoo.fr>
2024-05-23 17:58:20 +0200

    gros chgmt fichier et chemin d'accès

--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -19,36 +19,36 @@ public class ExerciseRunner {
             case 1:
                 // Python
                 fileName = "exo1_utilisateur.py";
-                FileModifier.writeToFile(fileName, code);
-                fileNames = new String[]{"Exercices/ExoPython/exo1_utilisateur.py"};
+                FileModifier.writeToFile(fileName, code,1, languageChoice);
+                fileNames = new String[]{"Exercices/ExoPython/Exo1PY/exo1_utilisateur.py"};
                 CodeRunner.runCodeFromFiles(fileNames);
                 break;
             case 2:
                 // Java
                 fileName = "exo1_utilisateur.java";
-                FileModifier.writeToFile(fileName, code);
-                fileNames = new String[]{"Exercices/ExoJava/exo1_utilisateur.java"};
+                FileModifier.writeToFile(fileName, code,1,languageChoice);
+                fileNames = new String[]{"Exercices/ExoJava/Exo1Java/exo1_utilisateur.java"};
                 CodeRunner.runCodeFromFiles(fileNames);
                 break;
             case 3:
                 // Javascript
                 fileName = "exo1_utilisateur.js";
-                FileModifier.writeToFile(fileName, code);
-                fileNames = new String[]{"Exercices/ExoJs/exo1_utilisateur.js"};
+                FileModifier.writeToFile(fileName, code,1,languageChoice);
+                fileNames = new String[]{"Exercices/ExoJs/Exo1JS/exo1_utilisateur.js"};
                 CodeRunner.runCodeFromFiles(fileNames);
                 break;
             case 4:
                 // PHP
                 fileName = "exo1_utilisateur.php";
-                FileModifier.writeToFile(fileName, code);
-                fileNames = new String[]{"Exercices/ExoPHP/exo1_utilisateur.php"};
+                FileModifier.writeToFile(fileName, code,1,languageChoice);
+                fileNames = new String[]{"Exercices/ExoPHP/Exo1PHP/exo1_utilisateur.php"};
                 CodeRunner.runCodeFromFiles(fileNames);
                 break;
             case 5:
                 // C
                 fileName = "exo1_utilisateur.c";
-                FileModifier.writeToFile(fileName, code);
-                fileNames = new String[]{"Exercices/ExoC/exo1_utilisateur.c"};
+                FileModifier.writeToFile(fileName, code,1,languageChoice);
+                fileNames = new String[]{"Exercices/ExoC/Exo1C/exo1_utilisateur.c"};
                 CodeRunner.runCodeFromFiles(fileNames);
                 break;
             default:
@@ -63,36 +63,36 @@ public class ExerciseRunner {
             case 1:
                 // Python
                 fileName = "exo2_utilisateur.py";
-                FileModifier.writeToFile(fileName, code);
-                fileNames = new String[]{"Exercices/ExoPython/exo2_utilisateur.py"};
+                FileModifier.writeToFile(fileName, code,2,languageChoice);
+                fileNames = new String[]{"Exercices/ExoPython/Exo2PY/exo2_utilisateur.py"};
                 CodeRunner.runCodeFromFiles(fileNames);
                 break;
             case 2:
                 // Java
                 fileName = "exo2_utilisateur.java";
-                FileModifier.writeToFile(fileName, code);
-                fileNames = new String[]{"Exercices/ExoJava/exo2_utilisateur.java"};
+                FileModifier.writeToFile(fileName, code,2,languageChoice);
+                fileNames = new String[]{"Exercices/ExoJava/Exo2Java/exo2_utilisateur.java"};
                 CodeRunner.runCodeFromFiles(fileNames);
                 break;
             case 3:
                 // Javascript
                 fileName = "exo2_utilisateur.js";
-                FileModifier.writeToFile(fileName, code);
-                fileNames = new String[]{"Exercices/ExoJs/exo2_utilisateur.js"};
+                FileModifier.writeToFile(fileName, code,2,languageChoice);
+                fileNames = new String[]{"Exercices/ExoJs/Exo2JS/exo2_utilisateur.js"};
                 CodeRunner.runCodeFromFiles(fileNames);
                 break;
             case 4:
                 // PHP
                 fileName = "exo2_utilisateur.php";
-                FileModifier.writeToFile(fileName, code);
-                fileNames = new String[]{"Exercices/ExoPHP/exo2_utilisateur.php"};
+                FileModifier.writeToFile(fileName, code,2,languageChoice);
+                fileNames = new String[]{"Exercices/ExoPHP/Exo2PHP/exo2_utilisateur.php"};
                 CodeRunner.runCodeFromFiles(fileNames);
                 break;
             case 5:
                 // C
                 fileName = "exo2_utilisateur.c";
-                FileModifier.writeToFile(fileName, code);
-                fileNames = new String[]{"Exercices/ExoC/exo2_utilisateur.c"};
+                FileModifier.writeToFile(fileName, code,2,languageChoice);
+                fileNames = new String[]{"Exercices/ExoC/Exo2C/exo2_utilisateur.c"};
                 CodeRunner.runCodeFromFiles(fileNames);
                 break;
             default:
--- FileModifier.java
+++ FileModifier.java
@@ -3,14 +3,67 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileModifier {
-    public static void writeToFile(String fileName, String content) {
-        String filePath = "Exercices/Exo1/" + fileName; // Chemin du fichier
+    public static void writeToFile(String fileName, String content, int exerciseChoice, int languageChoice) {
+        String filePath;
+        switch (languageChoice) {
+            case 1:
+                // Python
+                filePath = "Exercices/ExoPython/";
+                break;
+            case 2:
+                // Java
+                filePath = "Exercices/ExoJava/";
+                break;
+            case 3:
+                // Javascript
+                filePath = "Exercices/Exo1/";
+                break;
+            case 4:
+                // PHP
+                filePath = "Exercices/ExoPHP/";
+                break;
+            case 5:
+                // C
+                filePath = "Exercices/ExoC/";
+                break;
+            default:
+                System.out.println("Langage non pris en charge.");
+                return;
+        }
+        switch (exerciseChoice) {
+            case 1:
+                filePath += "exo1_utilisateur." + getFileExtension(languageChoice);
+                break;
+            case 2:
+                filePath += "exo2_utilisateur." + getFileExtension(languageChoice);
+                break;
+            default:
+                System.out.println("Exercice non pris en charge.");
+                return;
+        }
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
             writer.write(content);
             System.out.println("Le code a été enregistré dans le fichier : " + filePath);
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("Erreur lors de l'enregistrement du fichier.");
+        }
+    }
+
+    private static String getFileExtension(int languageChoice) {
+        switch (languageChoice) {
+            case 1:
+                return "py";
+            case 2:
+                return "java";
+            case 3:
+                return "js";
+            case 4:
+                return "php";
+            case 5:
+                return "c";
+            default:
+                return "";
         }
     }
 }
