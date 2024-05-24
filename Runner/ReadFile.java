78bccb1b527e568be6ab75f2140a0a2bb8af9085
Hugo <hugorivierre@yahoo.fr>
2024-05-24 22:27:56 +0200

    ok

--- Runner/ReadFile.java
+++ Runner/ReadFile.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-package Runner;
 public class ReadFile extends AbstractExerciseRunner {
 
     public ReadFile(String fileExtension, String filePath) {
