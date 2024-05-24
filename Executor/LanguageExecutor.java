0ae31e4a6a9172ff554d1148b21d39fff601c495
LoloLeRigaulO <loickenmogne16@gmail.com>
2024-05-24 22:30:39 +0200

    correction du dossier executor

--- Executor/LanguageExecutor.java
+++ Executor/LanguageExecutor.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+
+
 public interface LanguageExecutor {
     void compile(String filePath) throws IOException, InterruptedException;
     void execute(String filePath) throws IOException, InterruptedException;
