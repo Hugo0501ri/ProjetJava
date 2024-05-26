eb9e6f075237c02c3c7a1d89b2c8fa02bacc3ecf
Gabriel <gabrielbrossat@gmail.com>
2024-05-27 01:18:47 +0200

    update

--- Classes/Main.java
+++ Classes/Main.java
@@ -10,19 +10,30 @@ import javax.script.ScriptException;
     
     public class Main {
     
+          /**
+     * The main method that runs the application.
+     * This method initializes the application, processes user-submitted code,
+     * integrates it with provided exercise templates, executes the combined code,
+     * and compares the output with expected results.
+     *
+     * @param args Command-line arguments.
+     * @throws IOException If an I/O error occurs during file operations.
+     * @throws ScriptException If an error occurs while executing scripts.
+     * @throws InterruptedException If the thread is interrupted while waiting for processes to complete.
+     */
         public static void main(String[] args) throws IOException, ScriptException, InterruptedException {
             Scanner scanner = new Scanner(System.in);
             StringBuilder userCode = new StringBuilder();
     
-            // Choix de l'exercice
+            // Choice of exercise
             System.out.println("Choisissez l'exercice :");
             System.out.println("1. Exercice 1");
             System.out.println("2. Exercice 2");
     
             int exerciseChoice = scanner.nextInt();
-            scanner.nextLine(); // Pour consommer la nouvelle ligne restante
+            scanner.nextLine(); // To consume the remaining new line
     
-            // Choix du langage
+            // Choice of language
             System.out.println("Choisissez le langage :");
             System.out.println("1. Python");
             System.out.println("2. Java");
@@ -31,9 +42,9 @@ import javax.script.ScriptException;
             System.out.println("5. C");
     
             int languageChoice = scanner.nextInt();
-            scanner.nextLine(); // Pour consommer la nouvelle ligne restante
+            scanner.nextLine(); // To consume the remaining new line
     
-            // Récupération du code saisi par l'utilisateur
+            // Retrieve code entered by user
             System.out.println("Veuillez saisir votre code (appuyez sur Entrée pour terminer la saisie) :");
             while (true) {
                 String line = scanner.nextLine();
@@ -42,23 +53,22 @@ import javax.script.ScriptException;
                 }
                 userCode.append(line).append("\n");
             }
-    
-            // Création de l'instance ExerciseRunnerInterface en fonction du choix de langage
+            //Create ExerciseRunnerInterface instance according to language choice
             ExerciseRunnerInterface runner = ExerciseRunner.getRunner(languageChoice);
             if (runner == null) {
                 System.out.println("Langage non pris en charge.");
                 return;
             }
     
-            // Extraction de la fonction à partir du code utilisateur
+            // Extract function from user code
             String userFunction = ExerciseRunner.extractFunction(userCode.toString(), languageChoice);
             int userOutput = ExerciseRunner.callFunction(userFunction, new int[]{}, languageChoice);
     
-            // Lecture du code original
+             // Read original code
             String originalCodePath = ExerciseRunner.getOriginalFilePath(exerciseChoice, languageChoice);
             String originalCode = FileModifier.readFileAsString(originalCodePath);
     
-            // Comparaison des sorties des fonctions avec des valeurs aléatoires
+            // Comparison of function outputs with random values
             boolean isCorrect = ExerciseRunner.runAndCompare(exerciseChoice, languageChoice, userFunction, originalCode);
     
             if (isCorrect) {
